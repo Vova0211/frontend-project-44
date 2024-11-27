1993590c2779932dedfd505199198157395bfd73
Vova0211 <chernousov_07@bk.ru>
2024-11-27 15:54:28 +0300

    fix brain-even

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -10,22 +10,19 @@ for (let i = 0; i < 3; i++) {
     const random = Math.floor(Math.random() * 101);
     console.log(`Question: ${random}`);
     const answer = readlineSync.question('Your answer: ');
+    let ans;
+    // let res = Boolean;
+    if (answer === 'no') {
+      ans = 'yes';
+      // res = false;
+    } else {
+      ans = 'no';
+      // res = true;
+    }
     if ((answer === 'yes' && random % 2 === 0) || (answer === 'no' && random % 2 !== 0)) {
       console.log('Correct!');
-    } else if (answer === 'yes') {
-      console.log("'yes' is wrong answer ;(. Correct answer was 'no'.");
-      console.log(`Let's try again, ${name}!`);
-      break;
-    } else if (answer === 'no') {
-      console.log("'no' is wrong answer ;(. Correct answer was 'yes'.");
-      console.log(`Let's try again, ${name}!`);
-      break;
-    } else if (random % 2 === 0){
-      console.log(`'${answer}' is wrong answer ;(. Correct answer was 'yes'.`);
-      console.log(`Let's try again, ${name}!`);
-      break;
     } else {
-      console.log(`'${answer}' is wrong answer ;(. Correct answer was 'no'.`);
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${ans}'.`);
       console.log(`Let's try again, ${name}!`);
       break;
     }
